e6f883c083d881575a8a77d8d9fc25730ba98952
DenisDov <dovzdenis@gmail.com>
2023-02-08 15:00:39 +0200

    upd

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,21 +1,12 @@
 import { FontAwesome } from "@expo/vector-icons";
 
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-} from "@react-navigation/native";
+import { NavigationContainer, DarkTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
-import { ColorSchemeName, Pressable } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-
-import useColorScheme from "../hooks/useColorScheme";
-import ModalScreen from "../screens/Home/ModalScreen";
 
 import TabOneScreen from "../screens/Home/TabOneScreen";
 import TabTwoScreen from "../screens/Home/TabTwoScreen";
-import { RootStackParamList } from "./types";
 import LoginScreen from "../screens/Auth/Login";
 import FeedScreen from "../screens/Home/FeedScreen";
 import ArticleScreen from "../screens/Home/ArticleScreen";
@@ -29,69 +20,30 @@ export default function Navigation() {
   );
 }
 
-/**
- * A root stack navigator is often used for displaying modals on top of all other content.
- * https://reactnavigation.org/docs/modal
- */
-
 const Drawer = createDrawerNavigator();
 
-function MyDrawer() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={FeedScreen} />
-      <Drawer.Screen name="Article" component={ArticleScreen} />
-    </Drawer.Navigator>
-  );
-}
+// function MyDrawer() {
+//   return (
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Feed" component={FeedScreen} />
+//       <Drawer.Screen name="Article" component={ArticleScreen} />
+//     </Drawer.Navigator>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
 
 function RootNavigator() {
-  const userToken = "JWT 123";
-  console.log("userToken: ", userToken);
-  return (
+  const isAuth = true;
+  return isAuth ? (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={FeedScreen} />
+      <Drawer.Screen name="Article" component={ArticleScreen} />
+    </Drawer.Navigator>
+  ) : (
     <Stack.Navigator>
-      {userToken ? (
-        <Stack.Screen name="Home" component={MyDrawer} />
-      ) : (
-        <Stack.Group>
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={
-              {
-                // title: "Login",
-                // When logging out, a pop animation feels intuitive
-                // You can remove this if you want the default 'push' animation
-                // animationTypeForReplace: "pop",
-              }
-            }
-          />
-          <Stack.Screen
-            name="Register"
-            component={RegisterScreen}
-            options={
-              {
-                // title: "Register",
-              }
-            }
-          />
-        </Stack.Group>
-      )}
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
-    // <Stack.Navigator>
-    //   <Stack.Screen
-    //     name="Root"
-    //     component={BottomTabNavigator}
-    //     options={{ headerShown: false }}
-    //   />
-
-    //   <Stack.Group screenOptions={{ presentation: "modal" }}>
-    //     <Stack.Screen name="Modal" component={ModalScreen} />
-    //   </Stack.Group>
-    // </Stack.Navigator>
   );
 }
-
-// const BottomTab = createBottomTabNavigator<RootTabParamList>();
