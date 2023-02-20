6804d8b7594304cd3820aa3251926e444fa2df53
DenisDov <dovzdenis@gmail.com>
2023-02-20 13:21:12 +0200

    persist auth

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,14 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type AuthState = {
   isAuthenticated: boolean;
   setAuthenticated: (authenticated: boolean) => void;
 };
 
-const useAuthStore = create<AuthState>(set => ({
-  isAuthenticated: false,
-  setAuthenticated: (authenticated: boolean) =>
-    set({ isAuthenticated: authenticated }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set, get) => ({
+      isAuthenticated: false,
+      setAuthenticated: (authenticated: boolean) =>
+        set({ isAuthenticated: authenticated }),
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: state => ({ isAuthenticated: state.isAuthenticated }),
+    },
+  ),
+);
 
 export default useAuthStore;
