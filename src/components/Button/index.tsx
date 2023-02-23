8413d46d248ae251431c2ddea20c66655b97f037
DenisDov <dovzdenis@gmail.com>
2023-02-23 11:06:11 +0200

    basic btn comp

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,55 @@
+import { Text } from '@app/theme';
+import { useTheme } from '@shopify/restyle';
+import React from 'react';
+import { ActivityIndicator, Pressable, StyleSheet } from 'react-native';
+
+type Props = {
+  onPress: () => void;
+  label: string;
+  isLoading?: boolean;
+};
+
+const Button = ({ onPress, label, isLoading }: Props) => {
+  const theme = useTheme();
+  const RIPPLE_CONFIG = {
+    // color: theme.colors.mainActive,
+    borderless: true,
+    //   radius: 32,
+    //   foreground: false,
+  };
+  return (
+    <Pressable
+      disabled={isLoading}
+      onPress={onPress}
+      hitSlop={16}
+      android_ripple={RIPPLE_CONFIG}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'red' : theme.colors.main,
+        },
+        styles.button,
+      ]}>
+      {isLoading ? (
+        <ActivityIndicator color="white" />
+      ) : (
+        <Text color="btnText">{label}</Text>
+      )}
+    </Pressable>
+  );
+};
+
+export { Button };
+
+const styles = StyleSheet.create({
+  button: {
+    height: 48,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    shadowColor: '#0B0B0B',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 2,
+    elevation: 2,
+  },
+});
